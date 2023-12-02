0edb68396f9df22be4ed52690cbcc0fa812d5927
Daif Hamdan <114764621+Daif2811@users.noreply.github.com>
2023-12-02 11:50:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container')
+
+function photos (image){
+    img.src = image;
+}
+
+function background(color){
+    container.style.background =color;
+}
